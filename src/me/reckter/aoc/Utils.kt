5a4702ee9313b6b4d34345076e5dbe4b929e2a0e
Hannes <hannes@guedelhoefer.de>
2017-12-11 10:34:19 +0100

    :sparkles: Add download (and caching) of inputs

--- src/me/reckter/aoc/Utils.kt
+++ src/me/reckter/aoc/Utils.kt
@@ -1,12 +1,33 @@
 package me.reckter.aoc
 
+import khttp.get
 import java.io.File
 import java.nio.file.Files
 
 fun loadInput(problem: Int, part: Int = 0): List<String> {
+
     if (part != 0)
-        return readLines("${problem}_$part.txt")
-    return readLines("$problem.txt")
+        return readLines("input/${problem}_$part.txt")
+
+    if(!Files.exists(File("input/$problem.txt").toPath())) {
+        println("downloading file for input $problem...")
+
+        val sessionFile = File("session-id.txt")
+        if(!sessionFile.exists()) {
+            error("input not there and could not download file, because session-id.txt is missing!")
+        }
+
+        val response = get(
+            url = "http://adventofcode.com/2017/day/$problem/input",
+            cookies = mapOf("session" to sessionFile.readText())
+        )
+
+        val content = response.text
+        File("input/$problem.txt").writeText(content)
+        println("download done")
+    }
+
+    return readLines("input/$problem.txt")
 }
 
 fun readLines(file: String): List<String> {
